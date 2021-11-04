0c930c3b74ceed76698823998ab0bb0a59c6b0f9
Lizzie Anthony <lizziewrab@me.com>
2021-11-04 15:36:44 -0500

    commit tree 1d95561991c6e2e2ef6bdf79a2d885ba8e7d0167

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+// function superbowlWin(record) {
+//     if(record.result === 'W') {
+//     return record.year;
+// }
+// }
+
+// function superbowlWin(record) {
+//     if(record.result === "W") {
+//         return record.find(superbowlWin).year;
+//     } else {
+//         return undefined
+//     }
+// }
+
+function superbowlWin(array) {
+    let winningYear = array.find((array) => array.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return winningYear
+    }
+  }
+  
